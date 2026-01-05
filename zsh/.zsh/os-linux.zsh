1465daac06b977bca4ba53bd8097207d9cd09b8c
vector-ops <85575949+vector-ops@users.noreply.github.com>
2026-01-05 13:05:10 +0530

    add os specific config

---
+++ zsh/.zsh/os-linux.zsh
@@ -0,0 +1,17 @@
+# go binary
+export GOPATH=$HOME/go
+export PATH=$PATH:/usr/local/go/bin 
+
+# zsh plugins
+source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# NVM init
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# package installer aliases
+alias gimme='sudo dnf install -y'
+alias hab="sudo dnf search"
+alias lowdiff="sudo dnf remove -y"
